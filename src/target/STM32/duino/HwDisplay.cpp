68ad8ff91d2610a621e3066f2ac3f3912e6fc221
Paolo Calao <paolo.calao@gmail.com>
2020-09-13 20:09:48 +0200

    Add support for both SH1106 and SSD1306 display

--- src/target/STM32/duino/HwDisplay.cpp
+++ src/target/STM32/duino/HwDisplay.cpp
@@ -3,7 +3,19 @@
 #include <Wire.h>
 
 #define I2C
-#define DISPLAY_ADDRESS (0x78)
+#define SH1106
+
+#define DISPLAY_ADDRESS (0x3C)
+
+#if defined (SSD1306)
+#define DISPLAY_COLUMNS (128)
+#define DISPLAY_STARTCOL (0)
+#elif defined (SH1106)
+/* The screen is actually 130x64, so 2 cols must be cleared */
+#define DISPLAY_COLUMNS (130)
+#define DISPLAY_STARTCOL (2)
+#endif
+
 #define INIT_COMMANDS_SIZE (28)
 /*
  * Command sequence needed to correctly initialize the oled screen
@@ -15,46 +27,38 @@ static uint8_t INIT_COMMANDS[] = {0xAE, 0x20, 0x02, 0xB0, 0xC8, 0x00,
 										0xA4, 0xD3, 0x00, 0xD5, 0xF0, 0xD9, 0x22, 0xDA, 0x12,
 										0xDB, 0x20, 0x8D, 0x14, 0xAF};
 
-uint8_t addressConfig[] = {0x21, 0, 127, 0x22, 0, 7};
-
-//TwoWire i2c = TwoWire(PB_9, PB_8);
-//TwoWire i2c = TwoWire();
 
 void hwDisplaySetup()
 {
 #ifdef I2C
-	//i2c.begin();	
 	Wire.begin();	
-
 	delay(200);
-	//HAL_Delay(200);
+
 	/* Send display configuration through I2C */
-	//HAL_I2C_Mem_Write(&hi2c1, 0x78, 0x00, 1, INIT_COMMANDS, INIT_COMMANDS_SIZE, HAL_MAX_DELAY);
-	//i2c.beginTransmission(DISPLAY_ADDRESS);
 	Wire.beginTransmission(DISPLAY_ADDRESS);
+    Wire.write(0x00);
+    Wire.write(INIT_COMMANDS, INIT_COMMANDS_SIZE);
+	Wire.endTransmission();
 	/* Clear the whole display */
-	/* The screen is actually 130x64, so 2 more cols must be cleared */
-	uint8_t tempBuff[130];
-	for(int index = 0; index < 130; index++)
+	uint8_t tempBuff[DISPLAY_COLUMNS];
+	for(int index = 0; index < DISPLAY_COLUMNS; index++)
 	{
 		tempBuff[index] = 0;
 	} 
 	/* Send the black screen to clear the display */
 	for(int page = 0; page < 8; page++) {
 		uint8_t setPageCmd[] = {0xB0 + page, 0x00, 0x10};
+        Wire.beginTransmission(DISPLAY_ADDRESS);
 		Wire.write(0x00);
 		Wire.write(setPageCmd, 3);
+        Wire.endTransmission();
+
+        Wire.beginTransmission(DISPLAY_ADDRESS);
 		Wire.write(0x40);
-		Wire.write(tempBuff, 130);
-		//i2c.write(0x00);
-		//i2c.write(setPageCmd, 3);
-		//i2c.write(0x40);
-		//i2c.write(tempBuff, 130);
-		//HAL_I2C_Mem_Write(&hi2c1, 0x78, 0x00, 1, setPageCmd, 3, HAL_MAX_DELAY);
-		//HAL_I2C_Mem_Write(&hi2c1, 0x78, 0x40, 1, tempBuff, 130, HAL_MAX_DELAY);
+		Wire.write(tempBuff, DISPLAY_COLUMNS);
+        Wire.endTransmission();
 	}
-	Wire.endTransmission();
-#else
+#else //I2c
 
 #endif
 }
@@ -62,20 +66,18 @@ void hwDisplaySetup()
 void hwDisplaySend(uint8_t* buffer)
 {
 #ifdef I2C
-	Wire.beginTransmission(DISPLAY_ADDRESS);
-	//while(!i2c.available());
-	//i2c.write(buffer, 512);
-	/* Transfer the screen to the display */
 	for(int page = 0; page < 8; page++) {
-		uint8_t setPageCmd[] = {0xB0 + page, 0x00, 0x10};
+		uint8_t setPageCmd[] = {0xB0 + page, DISPLAY_STARTCOL, 0x10};
+        Wire.beginTransmission(DISPLAY_ADDRESS);
 		Wire.write(0x00);
 		Wire.write(setPageCmd, 3);
+        Wire.endTransmission();
+
+        Wire.beginTransmission(DISPLAY_ADDRESS);
 		Wire.write(0x40);
 		Wire.write(buffer + (page * 128), 128);
-		//HAL_I2C_Mem_Write(&hi2c1, 0x78, 0x00, 1, setPageCmd, 3, HAL_MAX_DELAY);
-		//HAL_I2C_Mem_Write(&hi2c1, 0x78, 0x40, 1, buffer + (page * 128), 128, HAL_MAX_DELAY);
+        Wire.endTransmission();
 	}
-	Wire.endTransmission();
 #else
 
 #endif
