d961cfcf9c176d84e80bff7b2c430d703ad25552
Paolo Calao <paolo.calao@gmail.com>
2020-09-13 20:13:37 +0200

    Add support for addressing display with ascending y
    By defining DISPLAY_ASCENDING_Y it is possible to select the bottom left corner of the display as (0, 0) rather than (0, 63).
    In this way the y coordinates can be addressed in an ascending order from bottom to top.

--- src/drivers/Display.cpp
+++ src/drivers/Display.cpp
@@ -29,6 +29,15 @@ void Display::send()
 
 void Display::draw(uint8_t x, uint8_t y, uint8_t color)
 {
+#if defined(DISPLAY_ASCENDING_Y)
+	uint8_t row = (7 - (uint8_t)y / 8);
+	if (color == 0)
+		_buffer[(row*128) + (uint8_t)x] |= 1 << (7 - ((uint8_t)y % 8));
+	else if (color == 1)
+		_buffer[(row*128) + (uint8_t)x] &= ~ ( 1 << (7 - (uint8_t)y % 8) );
+	else if (color == 2)
+		_buffer[(row*128) + (uint8_t)x] ^=  ( 1 << (7 - (uint8_t)y % 8) );
+#else 
 	uint8_t row = (uint8_t)y / 8;
 	if (color == 0)
 		_buffer[(row*128) + (uint8_t)x] |= 1 << ((uint8_t)y % 8);
@@ -36,4 +45,5 @@ void Display::draw(uint8_t x, uint8_t y, uint8_t color)
 		_buffer[(row*128) + (uint8_t)x] &= ~ ( 1 << ((uint8_t)y % 8) );
 	else if (color == 2)
 		_buffer[(row*128) + (uint8_t)x] ^=  ( 1 << ((uint8_t)y % 8) );
+#endif
 }
