ced8331fe9e0ebf5e241b9af09a6cc94fac5e4f8
Paolo Calao <paolo.calao@gmail.com>
2020-11-08 13:09:26 +0100

    Update Pool class
    Add allocated list, that contains all the allocated items.
    Allocated list can be retrieved to iterate through all the allocated items.
    Add _isAllocated bool value that indicates whether an item is in allocated or free list.

--- src/engine/Pool.h
+++ src/engine/Pool.h
@@ -8,56 +8,95 @@ class Poolable {
     //virtual void init();
     //virtual void deinit();
 
-    void setNext(Poolable* next) { 
+    void setNext(Poolable* next) 
+    { 
       _next = next; 
     }
 
-    Poolable* getNext() { 
+    Poolable* getNext() 
+    { 
       return _next; 
+    }
+    
+    void setAllocated(bool isAllocated)
+    {
+      _isAllocated = isAllocated;
+    }
+
+    bool isAllocated() 
+    {
+      return _isAllocated;
     }
 
   private:
     Poolable* _next;
+    bool _isAllocated;
 };
 
 template <class T, int N>
 class Pool {
   public:
+
     Pool() 
     {
+      // Put all the items in the free list
       for (int i = 0; i < (N - 1); i++) {
         _pool[i].setNext(&_pool[i + 1]);
+        _pool[i].setAllocated(false);
       }
-      _head = &_pool[0];
+      _headFree = &_pool[0];
       _pool[N - 1].setNext(NULL);
+      // Allocated list is empty
+      _headAllocated = NULL;
     }
 
     virtual ~Pool() {}
 
     T& newItem()
     {
-      if (!_head) {
-        while(1);
-      }
-      T& item = *_head;
-      _head = static_cast<T*>(item.getNext());
-      item.setNext(NULL);
-      //item.init();
+      // TODO: check if the pool is full
+      //if (!_headFree) {
+        //while(1);
+      //}
+
+      // Remove from the free list
+      T& item = *_headFree;
+      _headFree = static_cast<T*>(item.getNext());
+
+      // Add to the allocated list
+      item.setNext(_headAllocated);
+      _headAllocated = &item;
+      item.setAllocated(true);
+
+      //item.init()
       return item;
     }
 
     void deleteItem(T& item)
     {
-      //item.deinit();
-      item.setNext(_head);
-      _head = &item;
+      // TODO: check if the item is already in the free list
+      //if (!item.isAllocated()) return;
+      item.setAllocated(false);
+
+      // Remove from the allocated list
+      _headAllocated = static_cast<T*>(item.getNext());
+
+      // Add to the free list
+      item.setNext(_headFree);
+      _headFree = &item;
     }
 
     void clear() {}
 
+    T* getItems()
+    {
+      return _headAllocated;
+    }
+
   protected:
     T _pool[N];
-    T* _head;
+    T* _headFree;
+    T* _headAllocated;
 };
 
 #endif
