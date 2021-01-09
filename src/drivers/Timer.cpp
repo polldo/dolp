de7a9834d4e3e4e6e072869a48019928c2eb9a77
Paolo Calao <paolo.calao@gmail.com>
2021-01-09 10:41:32 +0100

    Introduce timeouts

--- src/drivers/Timer.cpp
+++ src/drivers/Timer.cpp
@@ -6,6 +6,10 @@ Timer::Timer() :
 	_secTick(0),
 	_seconds(0)
 {
+	for (int i = 0; i < NUM_TIMEOUTS; i++) {
+		_timeouts[i].assigned = false;
+		_timeouts[i].ticks = 0;
+	}	
 }
 
 Timer::~Timer()
@@ -38,4 +42,45 @@ void Timer::waitEndFrame()
 		_secTick = 0;
 		++_seconds;
 	}
+	// Update timeouts
+	updateTimeout();
+}
+
+int Timer::newTimeout()
+{
+	// Timeout 0 is reserved
+	for (int i = 1; i < NUM_TIMEOUTS; i++) {
+		if (!_timeouts[i].assigned) {
+			_timeouts[i].assigned = true;
+			return i;
+		}
+	}
+	return -1;
+}
+
+void Timer::deleteTimeout(int id)
+{
+	if (_timeouts[id].assigned) {
+		_timeouts[id].assigned = false;
+		_timeouts[id].ticks = 0;
+	}
+}
+
+void Timer::updateTimeout()
+{
+	for (int i = 1; i < NUM_TIMEOUTS; i++) {
+		if (_timeouts[i].assigned && _timeouts[i].ticks > 0) {
+			_timeouts[i].ticks -= 1;
+		}
+	}
+}
+
+void Timer::refreshTimeout(int id, uint64_t time)
+{
+	_timeouts[id].ticks = time / FRAME_MSEC + 1;
+}
+
+bool Timer::checkTimeout(int id)
+{
+	return _timeouts[id].ticks == 1;
 }
--- src/drivers/Timer.h
+++ src/drivers/Timer.h
@@ -2,6 +2,19 @@
 #define REFRESH_H_
 
 #include <Common.h>
+#include <engine/Pool.h>
+
+#ifndef NUM_TIMEOUTS
+#define NUM_TIMEOUTS 100
+#endif
+
+struct Timeout {
+  //int id;
+  bool assigned;
+  //bool expired;
+  //uint64_t time;
+  uint8_t ticks;
+};
 
 class Timer {
 public:
@@ -14,11 +27,21 @@ public:
   uint64_t getMilliseconds();
   uint64_t getSeconds();
 
+  int newTimeout();
+  void deleteTimeout(int id);
+  void updateTimeout();
+  void refreshTimeout(int id, uint64_t time);
+  bool checkTimeout(int id);
+
 private:
   unsigned long long _tick;
   uint8_t _secTick;
   uint64_t _seconds;
   uint64_t _milliseconds;
+
+  Timeout _timeouts[NUM_TIMEOUTS];
+  //int timeouts[NUM_TIMEOUTS];
+  //bool timeouts
 };
 
 #endif /* REFRESH_H_ */
